ae58eb60ab4f7e555f60930207a933976e2204fd
Saulė Westra <saule.westra@student.nhlstenden.com>
2022-10-12 11:34:45 +0200

    Eerste versie voor project van eten is toegevoegd.

---
+++ eten.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="Utf-8">
+		<title>VOULEZ VOUS CAFÉ</title>
+		<link rel="stylesheet" type="text/css" href="ni_pr.css">
+	</head>
+	<body>
+		<div id="mainContainer">
+		
+		</div>
+	</body>
+</html>
